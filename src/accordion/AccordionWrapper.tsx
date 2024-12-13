9f5cd8e1488234a655b1d0fd998e4bf552e4722a
AbdelLyon <abdelamajidahamiane@gmail.com>
2024-12-13 01:45:09 +0100

    accordion

--- src/accordion/AccordionWrapper.tsx
+++ src/accordion/AccordionWrapper.tsx
@@ -5,12 +5,8 @@ import {
   AccordionItemProps,
 } from "@nextui-org/react";
 
-interface AccordionItemData extends Omit<AccordionItemProps, "content"> {
-  content: React.ReactNode;
-}
-
 interface AccordionWrapperProps extends AccordionProps {
-  items: AccordionItemData[];
+  items: AccordionItemProps[];
 }
 
 export const AccordionWrapper = ({
@@ -20,11 +16,9 @@ export const AccordionWrapper = ({
   return (
     <Accordion {...accordionProps}>
       {items.map((item) => {
-        const { content, ...itemProps } = item;
-
         return (
-          <AccordionItem {...itemProps} key={itemProps.key}>
-            {content}
+          <AccordionItem {...item} key={item.key}>
+            {item.content}
           </AccordionItem>
         );
       })}
