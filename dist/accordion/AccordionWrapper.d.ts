31c1bca25fae7ee4ebf6a91a3085ca5472c22875
AbdelLyon <abdelamajidahamiane@gmail.com>
2024-12-13 02:01:43 +0100

    accordion update

--- dist/accordion/AccordionWrapper.d.ts
+++ dist/accordion/AccordionWrapper.d.ts
@@ -1,6 +1,9 @@
 import { AccordionProps, AccordionItemProps } from '@nextui-org/react';
+interface ExtendedAccordionItemProps extends Omit<AccordionItemProps, "content"> {
+    content: React.ReactNode;
+}
 interface AccordionWrapperProps extends AccordionProps {
-    items: AccordionItemProps[];
+    items: ExtendedAccordionItemProps[];
 }
 export declare const AccordionWrapper: ({ items, ...accordionProps }: AccordionWrapperProps) => import("react/jsx-runtime").JSX.Element;
 export {};
--- src/accordion/AccordionWrapper.tsx
+++ src/accordion/AccordionWrapper.tsx
@@ -5,8 +5,14 @@ import {
   AccordionItemProps,
 } from "@nextui-org/react";
 
+// Étendre AccordionItemProps pour ajouter content comme ReactNode
+interface ExtendedAccordionItemProps
+  extends Omit<AccordionItemProps, "content"> {
+  content: React.ReactNode;
+}
+
 interface AccordionWrapperProps extends AccordionProps {
-  items: AccordionItemProps[];
+  items: ExtendedAccordionItemProps[];
 }
 
 export const AccordionWrapper = ({
