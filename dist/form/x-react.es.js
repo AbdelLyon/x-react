51a5aa4ff82e646399d3b13c17e2249c9bc0e5a9
AbdelLyon <abdelamajidahamiane@gmail.com>
2024-12-15 09:17:58 +0100

    update input

--- dist/form/x-react.es.js
+++ dist/form/x-react.es.js
@@ -6,7 +6,7 @@ import { cn as o } from "../utils/x-react.es.js";
 import { IconEye as M, IconEyeOff as P, IconMail as V } from "@tabler/icons-react";
 const q = w(
   ({
-    items: t,
+    items: l,
     groupClasses: e,
     itemClasses: a,
     label: b = "Select an option",
@@ -16,7 +16,7 @@ const q = w(
     const c = {
       base: "w-full",
       label: "text-medium font-semibold"
-    }, l = {
+    }, t = {
       base: "w-full",
       label: "text-small",
       wrapper: "",
@@ -33,7 +33,7 @@ const q = w(
           base: o(c.base, e == null ? void 0 : e.base),
           label: o(c.label, e == null ? void 0 : e.label)
         },
-        children: t.map((n) => {
+        children: l.map((n) => {
           var f, d, N;
           return /* @__PURE__ */ r.jsx(
             E,
@@ -41,22 +41,22 @@ const q = w(
               ...n,
               classNames: {
                 base: o(
-                  l.base,
+                  t.base,
                   a == null ? void 0 : a.base,
                   n.className
                 ),
                 label: o(
-                  l.label,
+                  t.label,
                   a == null ? void 0 : a.label,
                   (f = n.classNames) == null ? void 0 : f.label
                 ),
                 wrapper: o(
-                  l.wrapper,
+                  t.wrapper,
                   a == null ? void 0 : a.wrapper,
                   (d = n.classNames) == null ? void 0 : d.wrapper
                 ),
                 control: o(
-                  l.control,
+                  t.control,
                   a == null ? void 0 : a.control,
                   (N = n.classNames) == null ? void 0 : N.control
                 )
@@ -73,7 +73,7 @@ const q = w(
 q.displayName = "RadioGroup";
 const A = w(
   ({
-    items: t,
+    items: l,
     groupClasses: e,
     itemClasses: a,
     label: b = "Select options",
@@ -83,7 +83,7 @@ const A = w(
     const c = {
       base: "w-full",
       label: "text-medium font-semibold"
-    }, l = {
+    }, t = {
       base: "w-full",
       label: "text-small",
       wrapper: ""
@@ -99,7 +99,7 @@ const A = w(
           base: o(c.base, e == null ? void 0 : e.base),
           label: o(c.label, e == null ? void 0 : e.label)
         },
-        children: t.map((n) => {
+        children: l.map((n) => {
           var f, d;
           return /* @__PURE__ */ r.jsx(
             T,
@@ -107,17 +107,17 @@ const A = w(
               ...n,
               classNames: {
                 base: o(
-                  l.base,
+                  t.base,
                   a == null ? void 0 : a.base,
                   n.className
                 ),
                 label: o(
-                  l.label,
+                  t.label,
                   a == null ? void 0 : a.label,
                   (f = n.classNames) == null ? void 0 : f.label
                 ),
                 wrapper: o(
-                  l.wrapper,
+                  t.wrapper,
                   a == null ? void 0 : a.wrapper,
                   (d = n.classNames) == null ? void 0 : d.wrapper
                 )
@@ -134,20 +134,20 @@ const A = w(
 A.displayName = "CheckboxGroup";
 const B = w(
   ({
-    length: t = 6,
-    label: e = `${t} digits OTP`,
+    length: l = 6,
+    label: e = `${l} digits OTP`,
     labelClasses: a,
     containerClasses: b,
     ...p
   }, u) => /* @__PURE__ */ r.jsxs("div", { ref: u, className: o("flex flex-col", b), children: [
     e && /* @__PURE__ */ r.jsx("p", { className: o("text-default-500 text-small mb-2", a), children: e }),
-    /* @__PURE__ */ r.jsx(L, { length: t, ...p })
+    /* @__PURE__ */ r.jsx(L, { length: l, ...p })
   ] })
 );
 B.displayName = "InputOtp";
 const Q = w(
   ({
-    variant: t = "bordered",
+    variant: l = "bordered",
     color: e = "default",
     size: a = "md",
     radius: b = "md",
@@ -155,7 +155,7 @@ const Q = w(
     fullWidth: u = !0,
     isClearable: x = !1,
     isRequired: c = !1,
-    isReadOnly: l = !1,
+    isReadOnly: t = !1,
     isDisabled: n = !1,
     // Custom props
     containerClasses: f,
@@ -189,20 +189,19 @@ const Q = w(
       if (i === "email")
         return /* @__PURE__ */ r.jsx(V, { className: "pointer-events-none opacity-60", size: 20 });
     }, v = "w-full", O = {
-      ...s.classNames,
       inputWrapper: o(
         "border-1 bg-white dark:bg-background",
         {
-          "data-[hover=true]:border-outline group-data-[focus=true]:border-outline h-11 group-data-[focus=true]:bg-content1": t === "bordered"
+          "data-[hover=true]:border-outline group-data-[focus=true]:border-outline h-11 group-data-[focus=true]:bg-content1": l === "bordered"
         },
         (I = s.classNames) == null ? void 0 : I.inputWrapper
       )
     };
-    return console.log(s.classNames), /* @__PURE__ */ r.jsx("div", { className: o(v, f), children: /* @__PURE__ */ r.jsx(
+    return /* @__PURE__ */ r.jsx("div", { className: o(v, f), children: /* @__PURE__ */ r.jsx(
       W,
       {
         ref: y,
-        variant: t,
+        variant: l,
         color: e,
         size: a,
         radius: b,
@@ -210,10 +209,10 @@ const Q = w(
         fullWidth: u,
         isClearable: x,
         isRequired: c,
-        isReadOnly: l,
+        isReadOnly: t,
         isDisabled: n,
         validate: k,
-        classNames: O,
+        classNames: { ...O, ...s.classNames },
         endContent: R(),
         type: h,
         ...s
