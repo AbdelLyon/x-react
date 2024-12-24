19260feec7e1d29a9db054a45b337aa2a80de81d
AbdelLyon <abdelamajidahamiane@gmail.com>
2024-12-25 00:32:28 +0100

    skeleton datagrid

--- dist/datagrid/x-react.es.js
+++ dist/datagrid/x-react.es.js
@@ -48,17 +48,17 @@ const H = {
   bordered: {
     header: "bg-content2 border border-default-200",
     column: "bg-content2 py-4 h-12",
-    row: "py-4 border-b border-default-200 last:border-b-0 hover:bg-content2"
+    row: "py-4 border-b border-default-200 last:border-b-0 hover:bg-content2 h-12"
   },
   striped: {
     header: "bg-content2 border border-default-200",
     column: "bg-content2 py-4 h-12",
-    row: "py-4 even:bg-content2"
+    row: "py-4 even:bg-content2 h-12"
   },
   unstyled: {
     header: "bg-content2 border border-default-200",
     column: "bg-content2 py-4 h-12",
-    row: "py-4 hover:bg-content2"
+    row: "py-4 hover:bg-content2 h-12"
   }
 };
 function U(e) {
--- src/datagrid/DataGrid.tsx
+++ src/datagrid/DataGrid.tsx
@@ -79,17 +79,17 @@ export const GRIDVARIANT = {
   bordered: {
     header: "bg-content2 border border-default-200",
     column: "bg-content2 py-4 h-12",
-    row: "py-4 border-b border-default-200 last:border-b-0 hover:bg-content2",
+    row: "py-4 border-b border-default-200 last:border-b-0 hover:bg-content2 h-12",
   },
   striped: {
     header: "bg-content2 border border-default-200",
     column: "bg-content2 py-4 h-12",
-    row: "py-4 even:bg-content2",
+    row: "py-4 even:bg-content2 h-12",
   },
   unstyled: {
     header: "bg-content2 border border-default-200",
     column: "bg-content2 py-4 h-12",
-    row: "py-4 hover:bg-content2",
+    row: "py-4 hover:bg-content2 h-12",
   },
 } as const;
 
