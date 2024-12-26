6ad4cb5f08ed3826f98400dc7d252587139ea332
AbdelLyon <abdelamajidahamiane@gmail.com>
2024-12-26 20:06:24 +0100

    refacto

--- src/data/default.ts
+++ src/data/default.ts
@@ -1,6 +1,6 @@
 export const GRID_VARIANTS = {
   bordered: {
-    header: "bg-content2 border border-default-200",
+    header: "bg-content2 border border-default-200 bg-content2 rounded-md",
     column: "bg-content2 py-4",
     row: "py-4 border-b border-default-200 last:border-b-0 hover:bg-content2",
   },
