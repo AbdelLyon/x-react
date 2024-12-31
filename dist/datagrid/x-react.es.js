2c4ecffed10ebacec58f8a2268e53f7978863b04
AbdelLyon <abdelamajidahamiane@gmail.com>
2024-12-31 19:48:49 +0100

    update default variant grid

--- dist/datagrid/x-react.es.js
+++ dist/datagrid/x-react.es.js
@@ -41,18 +41,18 @@ const _ = ({
 }), w = {
   bordered: {
     thead: "bg-content2",
-    th: "py4 bg-content2 py-4 h-10",
-    tr: "py-4 border-b border-default last:border-b-0 hover:bg-content2 h-10"
+    th: "py4 bg-content2 py-4 h-12",
+    tr: "py-4 border-b border-default last:border-b-0 hover:bg-content2 h-12"
   },
   striped: {
     thead: "bg-content2",
-    th: "py4 bg-content2 py-4 h-10",
-    tr: "py-4 even:bg-content2 h-10"
+    th: "py4 bg-content2 py-4 h-12",
+    tr: "py-4 even:bg-content2 h-12"
   },
   unstyled: {
     thead: "bg-content2",
-    th: "py4 bg-content2 py-4 h-10",
-    tr: "py-4 hover:bg-content2 h-10"
+    th: "py4 bg-content2 py-4 h-12",
+    tr: "py-4 hover:bg-content2 h-12"
   }
 }, B = ({
   columns: e = 5,
