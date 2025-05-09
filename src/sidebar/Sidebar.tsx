a8e85964f55e6d5361877db982fe2200c55459c1
AbdelLyon <abdelamajidahamiane@gmail.com>
2025-05-09 23:40:08 +0200

    sidebar color

--- src/sidebar/Sidebar.tsx
+++ src/sidebar/Sidebar.tsx
@@ -170,7 +170,7 @@ export const Sidebar = ({
     <aside
       ref={ref}
       className={mergeTailwindClasses(
-        "fixed left-0 h-screen flex flex-col bg-[#191919] border-r border-border",
+        "fixed left-0 h-screen flex flex-col bg-[#191919e1] border-r border-border",
         {
           "w-[270px]": isDesktop,
           "w-[70px]": isTablet,
