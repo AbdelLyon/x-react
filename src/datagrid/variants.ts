81c5ee63f7405597301baae3b547a1c2215d58f7
AbdelLyon <abdelamajidahamiane@gmail.com>
2025-06-01 14:51:26 +0200

    datagrid

--- src/datagrid/variants.ts
+++ src/datagrid/variants.ts
@@ -2,7 +2,7 @@ export const GRID_VARIANTS = {
   bordered: {
     thead: "bg-content1-100 rounded-none",
     th: "h-14 bg-content1-100",
-    tr: "py-4 border-b border-border last:border-b-0 hover:bg-content1-200 dark:hover:bg-content1 h-12",
+    tr: "py-4 border-border hover:bg-content1-200 dark:hover:bg-content1 h-12",
     td: "py-4 h-12 max-h-12",
   },
   striped: {
